d29d3b096aef83068ef45c7fedf4e82ec78dd085
Hamidreza Bolandhemmat <hbolandhemmat@fb.com>
2018-10-30 12:07:54 -0700

    including the copy right and license files to some of the hidden files in the child folders

--- utilities/Aswing/run_aswing.m
+++ utilities/Aswing/run_aswing.m
@@ -1,3 +1,18 @@
+%   Copyright (C) 2018-present, Facebook, Inc.
+% 
+%   This program is free software; you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation; version 2 of the License.
+% 
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+% 
+%   You should have received a copy of the GNU General Public License along
+%   with this program; if not, write to the Free Software Foundation, Inc.,
+%   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%
 function run_aswing( aswing_opts )
 %run_aswing runs aswing to output both a reduced order model and bode data
 %at the desired conditions.  The outputs are saved to AF.bode, AF.e00, and
--- utilities/Aswing/sysID.m
+++ utilities/Aswing/sysID.m
@@ -1,3 +1,18 @@
+%   Copyright (C) 2018-present, Facebook, Inc.
+% 
+%   This program is free software; you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation; version 2 of the License.
+% 
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+% 
+%   You should have received a copy of the GNU General Public License along
+%   with this program; if not, write to the Free Software Foundation, Inc.,
+%   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%
 function [ss_id] = sysID( model_name, runSysIDReport, aswing_opts )
 %sysID Identifies state space model from ASWING frequency response data.
 % Model is saved to a mat file entitled ss_model_name.
